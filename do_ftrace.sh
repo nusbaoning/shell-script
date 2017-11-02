06e889fc6c7c2dced5e8187d5ae8eb68797708cb
nusbaoning <544124775@qq.com>
2017-11-02 13:23:54 +0800

    用ftrace收集filebench访问结果
    目前需要手动修改filebench中的run time和ftrace中的log名称

--- do_ftrace.sh
+++ do_ftrace.sh
@@ -2,9 +2,13 @@
 #baoning
 #08/08/2016
 #script to change to do_ftrace working directory and also set tracer
-cd /sys/kernel/debug/tracing
-echo "input the tracer you want"
-read tracer
-echo $tracer > current_tracer
-cat current_tracer
+dir=/sys/kernel/debug/tracing
+sudo echo > $dir/trace
+sudo echo 0 > $dir/tracing_on
+sudo cat $dir/trace
+sudo echo 1 > $dir/tracing_on
+sudo filebench -f fileserver.f
+echo 0 > $dir/tracing_on
+head $dir/trace
+cat $dir/trace > ./data/fileserver_1h.log
 
